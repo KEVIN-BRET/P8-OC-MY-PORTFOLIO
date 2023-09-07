85efe2b0137337c70278c93a53e7da0788002b37
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-09-07 21:30:28 +0200

    CSS du header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './Header.scss';
 import profilePhoto from '../../assets/images/profile_photo.png'
 // import { NavLink } from 'react-router-dom';
 
 export default function Header(props) {
+
+	// est-ce que la page est scrollée ?
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 0) {
+				setIsScrolled(true);
+				console.log('scroll');
+			} else {
+				setIsScrolled(false);
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
-		<div className='header'>
+		<div className={`header ${isScrolled ? 'header--scrolled' : ''}`}>
 
 			<div className='logo'>
 				<img className='profilePhoto' src={profilePhoto} alt="logo" />
@@ -17,12 +38,12 @@ export default function Header(props) {
 				<ul className='header__nav'>
 					<li>Accueil</li>
 					<li>Skills</li>
-					<li>Projets</li>
+					<li>Portfolio</li>
 					<li>Contact</li>
 				</ul>
 
-				<div className="cv">Mon CV</div>
-				
+				<div className="cv">Télécharger mon CV</div>
+
 			</div>
 
 		</div>
